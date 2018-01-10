cb92fcf495149fe7058a571ebf859199885ba7a6
Eric Hare <ericrhare@gmail.com>
2018-01-10 08:46:40 -0600

    Initial UI

--- app.R
+++ app.R
@@ -4,11 +4,22 @@ library(magick)
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
    
-   titlePanel("Magick Grids"),
+   titlePanel("Magick Images"),
    
    sidebarLayout(
       sidebarPanel(
-          h4("About")
+          h4("About"),
+          
+          HTML("This application provides a front-end to the <a href='https://cran.r-project.org/package=magick'>magick</a> package for manipulating images. Several pieces of functionality are supported, and the resulting image can be downloaded."),
+          
+          hr(),
+          
+          h4("Image"),
+          fileInput("image", "Upload Image", accept = c("image/gif", "image/jpeg", "image/png", "image/tiff")),
+          
+          hr(),
+          
+          h4("Configuration")
       ),
       
       mainPanel(
